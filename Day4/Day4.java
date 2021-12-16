aec6882137d3591373230fe7a992a5726b9303ae
yashkhanna <yashkhanna846@gmail.com>
2021-12-16 17:00:22 +0530

    Added README.md

---
+++ Day4/Day4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Day4 {
+	public static void main(String[] args) throws IOException {
+		f1("Day4-small.txt");
+        f1("Day4.txt");
+	}
+
+    private static void f1(String fileName) throws IOException {
+        File file = new File(fileName);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        
+    }
+
+    private static void f2(String fileName) throws IOException {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent Of Code
+
+Solutions to problems on http://adventofcode.com/2021 using Java
